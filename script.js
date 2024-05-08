0c48b5e4844ecb57d9a055440084a825bdb5a522
Mateus <mateusmpi@gmail.com>
2024-05-08 11:28:27 -0300

    fix: sort deck

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
 let numberSelected = "";
 
+const backCards = [
+    "bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot"
+];
+
+let deckCards = [];
 
 
-function clickedCard(cardClicked){
-    cardClicked.classList.toggle("back");
+function createDeck() {
+    for (let i = 0; i < numberSelected / 2; i++) {
+        deckCards.push(backCards[i]);
+        deckCards.push(backCards[i]);
+    }
+
+    deckCards.sort(comparador); 
+
+
+// Esta função pode ficar separada do código acima, onde você preferir
+function comparador() { 
+	return Math.random() - 0.5; 
 }
+
+}
+
+function clickedCard(cardClicked) {
+    cardClicked.classList.toggle("back");
+    
+    let cardName = cardClicked.id;
+    console.log(cardName);
+
+    if (!cardClicked.classList.contains("back")) {
+        
+        let templateCard = `
+            <img src="Arquivos Úteis - Projeto 04 - Parrot Card Game/${cardName}.gif" alt="${cardName}">
+            `;
+        cardClicked.innerHTML = templateCard;
+
+        }
+    } 
 
 function showCards() {
     const ul = document.querySelector("ul");
+    createDeck();
 
-    for ( let i = 0; i < numberSelected; i++) {
+    for (let i = 0; i < numberSelected; i++) {
         let template = `
-            <li class="card back" onclick="clickedCard(this)">
+            <li class="card back" id="${deckCards[i]}" onclick="clickedCard(this)">
                 <img src="Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt="Parrot card">
             </li>
         `;
@@ -19,13 +59,12 @@ function showCards() {
     }
 }
 
-
 function numberCards() {
-    
-    while (isNaN(numberSelected) || numberSelected < 4 || numberSelected > 14 || numberSelected % 2 === 1){
+
+    while (isNaN(numberSelected) || numberSelected < 4 || numberSelected > 14 || numberSelected % 2 === 1) {
         numberSelected = Number(prompt("Com quantas cartas quer jogar?"));
     }
-    
+
     return numberSelected;
 }
 
